b8cbc2d3abaf56c4ad28681aa21da04edbda3a9a
LocalML Team <productspace@proton.me>
2026-09-21 05:52:09 +0000

    fix: requirement.txt transitive dep upd

--- setup.py
+++ setup.py
@@ -17,12 +17,11 @@ setup(
             "omf=sentinel.cli.main:cli",
         ],
     },
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
